6f05d42bb7fd3f8cde0a4ef6cf0d054c1f488459
Isiphobooi <anelisiweb62@gmail.com>
2023-07-14 08:29:16 -0700

    Positive anyting is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,23 +3,24 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints a random number and states whether
+ *	  it is positive, negative, or zero.
  *
- * Return: Always 0 (Success)
+ *	  Return: Always 0.
  */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n = rand() -RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 
-	if (n == 0)
-		printf("%d is zero\n", n);
+	if (n > 0)
+		printf("%d is positive\n", n);
 	else if (n < 0)
-		printf("%d is negative\n" , n);
+		printf("%d is negative\n", n);
 	else
-		printf("%d is positive\n" , n);
-	return (0)
+		printf("%d is zero\n", n);
 
+	return (0);
 }
