87ca3be0a8779ba62ae91b30b43bad267b860b83
Isiphobooi <anelisiweb62@gmail.com>
2023-08-02 09:22:43 -0700

    Once an idea has taken hold of the brain it's almost impossible to eradicate

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,23 +1,18 @@
 #include "main.h"
+
 /**
- * _pow_recursion - Write a function that returns the value of x raised to the
- * power of y.
+ * _pow_recursion - raises x to the power of y
+ * @x: Number Integer
+ * @y: Power
  *
- * @x: This is the input number
- * @y: This is the power number
- *
- * Return: if "y" is bigger than zero raised to the power of "y",
- * return -1 if "y" is lower than zero, return 1 if "y" is equal to 1, 
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _pow_recursion(int x, int y)
 {
-	if (y == 0)
-	{
-		return (1);
-	}
 	if (y < 0)
-	{
 		return (-1);
-	}
+	if (y == 0)
+		return (1);
 	return (x * _pow_recursion(x, y - 1));
 }
