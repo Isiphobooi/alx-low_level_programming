c2c6a59b9f27463c7a3f8df00e5fb16ca52a4b5e
Isiphobooi <anelisiweb62@gmail.com>
2023-08-25 16:47:01 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,20 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
-  * print_listint - prints all elements of a list
-  *
-  * @h: list to print elements of
-  * Return: size_t of the list
+  * print_list - prints all elements of a list
+  * @h: a list to print elements of
+  * Return: the number of nodes in list
   */
-size_t print_listint(const listint_t *h)
+size_t print_list(const list_t *h)
 {
-	size_t n = 0;
+	int i = 0, ret = 1;
+	char nul[] = "(nil)";
 
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		n++;
-	}
-	return (n);
+	if (!h)
+		return (0);
+	if (h->str)
+		i = _strlen(h->str);
+	printf("[%d] ", i);
+
+	if (!h->str)
+		printf("%s\n", nul);
+	else
+		printf("%s\n", h->str);
+	if (h->next)
+		ret += print_list(h->next);
+	return (ret);
+}
+
+/**
+ * _strlen - finds string length
+ * @str: string
+ * Return: length of string
+ */
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; *(str + i);)
+		i++;
+	return (i);
 }
